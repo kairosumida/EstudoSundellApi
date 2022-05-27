187f1e134964879f91d5c89b0c8b70b222d57f0f
kairosumida <kairosumida@hotmail.com>
2022-05-27 12:30:06 -0300

    Inicio

---
+++ EstudoSundellApi/Model/Artigo.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EstudoSundellApi.Model
+{
+	public class Artigo
+	{
+		[JsonPropertyName("id")]
+		public string Id { get; set; }
+		public string Nome { get; set; }
+		public DateTime DataInicio { get; set; }
+		public DateTime DataTermino { get; set; }
+	}
+}
+
--- EstudoSundellApi/Program.cs
+++ EstudoSundellApi/Program.cs
@@ -1,4 +1,6 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
